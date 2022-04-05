f84e34157d29a6db7edf3697b48d304c56e3db27
yEsPaP <csgo40055@gmail.com>
2022-04-05 17:44:50 +0000

    A260GDDSCAUJ2

---
+++ system/bin/install-recovery.sh
@@ -0,0 +1,46 @@
+#!/system/bin/sh
+if ! applypatch -c EMMC:/dev/block/platform/13540000.dwmmc0/by-name/RECOVERY:22235680:fb2e5953c0de90a3f59aec04cb21713119a04051; then
+  applypatch EMMC:/dev/block/platform/13540000.dwmmc0/by-name/BOOT:17990176:e1326861a966a25555420bf5c1070c3e0274603b EMMC:/dev/block/platform/13540000.dwmmc0/by-name/RECOVERY fb2e5953c0de90a3f59aec04cb21713119a04051 22235680 e1326861a966a25555420bf5c1070c3e0274603b:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || echo 454 > /cache/fota/fota.status
+else
+  log -t recovery "Recovery image already installed"
+fi
+
+if [ -e /cache/recovery/command ] ; then
+  PACKAGE_PATH=""
+  SEARCH_COMMAND="--update_package"
+  PATH_POS=16
+  if [ -e '/system/bin/grep' ] ; then
+    PACKAGE_PATH=`cat /cache/recovery/command | grep 'update_package'`
+    PACKAGE_ORG_PATH=`cat /cache/recovery/command | grep 'update_org_package'`
+    if [ "$PACKAGE_ORG_PATH" != "" ] ; then
+      PACKAGE_PATH=$PACKAGE_ORG_PATH
+      SEARCH_COMMAND="--update_org_package"
+      PATH_POS=20
+    fi
+    if [ -e /cache/recovery/saved" ] ; then
+      rm -rf /cache/recovery/saved
+    fi
+
+    if [ -e /data/.recovery/saved" ] ; then
+      rm -rf /data/.recovery/saved
+    fi
+  fi
+  if [ "$PACKAGE_PATH" != "" ] ; then
+    for PACKAGE_LINE in $PACKAGE_PATH
+    do
+      if [ ${PACKAGE_LINE:0:$PATH_POS} == $SEARCH_COMMAND ] ; then
+        break
+      fi
+    done
+    let PATH_POS+=1
+    PACKAGE_PATH=${PACKAGE_LINE:$PATH_POS}
+    if [ "$PACKAGE_PATH" != "" ] ; then
+      rm $PACKAGE_PATH
+    fi
+  fi
+  if [ ${PACKAGE_PATH:0:5} == "/data" ] ; then
+    echo $PACKAGE_PATH > /cache/fota/fota_path_command
+    chown system:system /cache/fota/fota_path_command
+  fi
+  rm /cache/recovery/command
+fi
